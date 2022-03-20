97c30e4aeb2f91122f3862575fe3bf40a8b5cb8f
Divit <divitkalathil@gmail.com>
2022-03-20 15:57:55 +0530

    https://github.com/AyushAsawa0003/startup-dwaar

--- src/components/Screens/Founder.js
+++ src/components/Screens/Founder.js
@@ -1,66 +1,122 @@
 import React from "react";
+import 'C:/Users/Divit/Documents/Startup Dwaar/src/components/Screens/Founder/founder.css'
+import '../Screens/Founder/FounderDashboard.css'
+import YoutubeEmbed from '../assets/vids/YoutubeEmbed.js'
 
 const Founder = () => {
-  
-  return  <div>
-{/* { <header>
-<div class="page-banner home-banner mb-5">
-      <div class="slider-wrapper">
-        <div class="owl-carousel hero-carousel">
-          <div class="hero-carousel-item">
-            <img src="../assets/img/bg_image_1.jpg" alt=""/>
-            <div class="img-caption">
-              <div class="subhead">We're design studio believe in ideas</div>
-              <h1 class="mb-4">Creative Design</h1>
-              <a href="#services" class="btn btn-outline-light">Get Started</a>
-            </div>
-          </div>
-          <div class="hero-carousel-item">
-            <img src="../assets/img/bg_image_2.jpg" alt=""/>
-            <div class="img-caption">
-              <h1 class="mb-4">We combine Design, Thinking, and Technical</h1>
-              <a href="#services" class="btn btn-outline-light">Get Started</a>
-              <a href="#services" class="btn btn-primary">See Pricing</a>
-            </div>
-          </div>
-          <div class="hero-carousel-item">
-            <img src="../assets/img/bg_image_3.jpg" alt=""/>
-            <div class="img-caption">
-              <div class="subhead">Easy way to build perfect website</div>
-              <h1 class="mb-4">Beautify handcrafted templates for your website</h1>
-              <a href="#services" class="btn btn-primary">Read More</a>
-            </div>
-          </div>
+  return <div>
+
+  <div className="page-section">
+      <div className="container dashboard">
+        <div className="text-center">
+          <h1 className="dash-title">Services we provide for Entrepreneurs</h1>
+          <h2>See what can we do for your Business</h2>
         </div>
-      </div> 
-    </div> 
 
-    </header> }
-
-  { <main>
-  <div class="page-section">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-lg-6 py-3">
-            <div class="subhead">Founder</div>
-            <h2 class="title-section">We are <span class="fg-primary">Professional Teams</span> to Growth your Business</h2>
-
-            <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of</p>
-
-            <a href="about.html" class="btn btn-primary mt-4">Read More</a>
+        <div className="row justify-content-center">
+          <div className="col-md-6 col-lg-4 col-xl-3 py-3 mb-3">
+            <div className="text-center img-border">
+              <div className="img-fluid mb-4">
+                <img src={require("../assets/img/investors.jpg")} alt="" height="160px" width="220px"/>
+              </div>
+              <h5>Investors</h5>
+            </div>
           </div>
-          <div class="col-lg-6 py-3">
-            <div class="about-img">
-              <img src="C:\Users\Divit\Documents\Startup Dwaar\public\bg_image_1.png" alt=""/>
+          <div className="col-md-6 col-lg-4 col-xl-3 py-3 mb-3">
+            <div className="text-center">
+              <div className="img-fluid mb-4">
+              <img src={require("../assets/img/incubators.jpg")} alt="" height="160px" width="220px"/>
+              </div>
+              <h5>Incubators</h5>
+            </div>
+          </div>
+
+          <div className="col-md-6 col-lg-4 col-xl-3 py-3 mb-3">
+            <div className="text-center">
+              <div className="img-fluid mb-4">
+              <img src={require("../assets/img/mentors.jpg")} alt="" height="160px" width="220px"/>
+              </div>
+              <h5>Mentorship</h5>
+            </div>
+          </div>
+
+          <div className="col-md-6 col-lg-4 col-xl-3 py-3 mb-3">
+            <div className="text-center">
+              <div className="img-fluid mb-4">
+              <img src={require("../assets/img/jobs.jpg")} alt="" height="160px" width="220px"/>
+              </div>
+              <h5>Job Careers</h5>
             </div>
           </div>
         </div>
       </div>
-    </div>  */}
+    </div> 
 
 
-  {/* </main> } */}
 
+
+
+
+
+    <div className="faq_main"> 
+      <h2 className="faq_header">Frequently Asked Questions(FAQs)</h2>
+  <details open>
+  <summary>How to register your new Business?</summary>
+  <div className="faq__content">
+    <p>
+      <ol>
+        <li><h3>Get Digital Signature Certificates from the authorities:</h3>
+          <p>As the registration process of the company is completely online, Digital signatures are required to file the forms on the MCA portal. DSC is mandatory for all the proposed directors and the subscribers of the Memorandum of Association (MoA) and Articles of Association (AoA).</p>
+        </li>
+        <li> <h3>Direct Identification Number</h3>
+          <p>The Director Identification Number (DIN) is an identification number for a director and it has to be obtained by anyone who wants to be a director in a company. The DIN of all the proposed directors of the company along with the name and the address proof are to be provided in the company registration form. DIN can be obtained while filing the SPICe+ form, i.e. company registration form. </p>
+        </li>
+        <li>
+          <h3>Registration on the MCA Portal </h3>
+          <p>To apply for company registration, the SPICe+ form is to be filled out and submitted on the MCA portal. To fill out the SPICe+ form and submit documents, the director of the company has to register on the MCA portal. After registration, the director can log in and will obtain access to the MCA portal services which include filing e-forms and viewing public documents.</p>
+        </li>
+        <li>
+          <h3>Certificate of Incorporation</h3>
+          <p>Once, the registration application is filled and submitted along with the required documents, the Registrar of Companies will examine the application. Upon verification of the application, he will issue the Certificate of Incorporation of the Company.
+          </p>
+          <p>The Certificate of Incorporation is issued with PAN and TAN as allotted by the Income Tax Department. An electronic mail with Certificate of Incorporation as an attachment along with PAN and TAN will also be sent to the applicant.</p>
+        </li>
+      </ol>
+    </p>
+  </div>
+</details>
+<details>
+  <summary>How does Startup Dwaar help you grow your business?</summary>
+  <div className="faq__content">
+    <p>Startup Dwaar is a not for profit platform for engaging and innovative entrepreneurs providing a suitable environment for startups to bloom. 
+      We also have a network of diverse investors looking forward to invest capital and scale the new business, according to the market requirements.
+    </p>
+  </div>
+</details>
+<details>
+  <summary>How to find an angel investor for my startup?</summary>
+  <div className="faq__content">
+    <p>The best way to find investors is to find customers. If can you cant find customers, you will not be able to find investors. Once you see some traction, revenue and feel that you have a MSP ( minimum sellable product ), then you can start looking at investors. Note that early stage capital is very expensive and angel funds will take a significant chunk of your company. </p>
+  </div>
+</details>
+
+</div>
+
+<div>
+
+  <h2 className="Videos">Entrepreneurship Videos</h2>
+<div className="App">
+  <ol>
+      <li><h4>How to Start a Business, the Guide for Entrepreneurs</h4></li>
+      <YoutubeEmbed embedId="PkplQjXRWsk" />
+      
+      <li>
+      <h4>Top 5 Entrepreneurship Lessons</h4></li>
+      <YoutubeEmbed embedId="e1rEHiuDtuc" />
+      
+      </ol>
+    </div>
+  </div>
 
   </div>;
 };
--- src/components/Screens/JobSeeker.js
+++ src/components/Screens/JobSeeker.js
@@ -1,7 +1,229 @@
 import React from "react";
 
 const JobSeeker = () => {
-  return <div>JobSeeker</div>;
+  return <div>
+    <div class="page-section">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-10">
+            <div class="row">
+              <div class="col-md-6 col-lg-4 py-3">
+                <div class="card-blog">
+                  <div class="header">
+                    <div class="avatar">
+                      <img src="../assets/img/person/person_1.jpg" alt=""/>
+                    </div>
+                    <div class="entry-footer">
+                      <div class="post-author">Ayush Asawa</div>
+                      <a href="#" class="post-date">28 Apr 2021</a>
+                    </div>
+                  </div>
+                  <div class="body">
+                    <div class="post-title"><a href="blog-single.html">Solstice Computing</a></div>
+                    <div class="post-excerpt">Software Analyst</div>
+                  </div>
+                  <div class="footer">
+                    <a href="blog-single.html">Read More <span class="mai-chevron-forward text-sm"></span></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-4 py-3">
+                <div class="card-blog">
+                  <div class="header">
+                    <div class="avatar">
+                      <img src="../assets/img/person/person_1.jpg" alt=""/>
+                    </div>
+                    <div class="entry-footer">
+                      <div class="post-author">Pranav Aher</div>
+                      <a href="#" class="post-date">5 July 2021</a>
+                    </div>
+                  </div>
+                  <div class="body">
+                    <div class="post-title"><a href="blog-single.html">Ecotel Technologies</a></div>
+                    <div class="post-excerpt">Software Engineer</div>
+                  </div>
+                  <div class="footer">
+                    <a href="blog-single.html">Read More <span class="mai-chevron-forward text-sm"></span></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-4 py-3">
+                <div class="card-blog">
+                  <div class="header">
+                    <div class="avatar">
+                      <img src="../assets/img/person/person_1.jpg" alt=""/>
+                    </div>
+                    <div class="entry-footer">
+                      <div class="post-author">Vivek Shetty</div>
+                      <a href="#" class="post-date">5 Dec 2021</a>
+                    </div>
+                  </div>
+                  <div class="body">
+                    <div class="post-title"><a href="blog-single.html">Urban Tiffins</a></div>
+                    <div class="post-excerpt">Business Development</div>
+                  </div>
+                  <div class="footer">
+                    <a href="blog-single.html">Read More <span class="mai-chevron-forward text-sm"></span></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-4 py-3">
+                <div class="card-blog">
+                  <div class="header">
+                    <div class="avatar">
+                      <img src="../assets/img/person/person_1.jpg" alt=""/>
+                    </div>
+                    <div class="entry-footer">
+                      <div class="post-author">Archis Gune</div>
+                      <a href="#" class="post-date">15 Jan 2022</a>
+                    </div>
+                  </div>
+                  <div class="body">
+                    <div class="post-title"><a href="blog-single.html">Emtronic systems</a></div>
+                    <div class="post-excerpt">System Engineer</div>
+                  </div>
+                  <div class="footer">
+                    <a href="blog-single.html">Read More <span class="mai-chevron-forward text-sm"></span></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-4 py-3">
+                <div class="card-blog">
+                  <div class="header">
+                    <div class="avatar">
+                      <img src="../assets/img/person/person_1.jpg" alt=""/>
+                    </div>
+                    <div class="entry-footer">
+                      <div class="post-author">Bhavika Vaishnavi</div>
+                      <a href="#" class="post-date">3 Feb 2022</a>
+                    </div>
+                  </div>
+                  <div class="body">
+                    <div class="post-title"><a href="blog-single.html">Novel Technologies</a></div>
+                    <div class="post-excerpt">Marketing Analyst</div>
+                  </div>
+                  <div class="footer">
+                    <a href="blog-single.html">Read More <span class="mai-chevron-forward text-sm"></span></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-4 py-3">
+                <div class="card-blog">
+                  <div class="header">
+                    <div class="avatar">
+                      <img src="../assets/img/person/person_1.jpg" alt=""/>
+                    </div>
+                    <div class="entry-footer">
+                      <div class="post-author">Divit Kalathil</div>
+                      <a href="#" class="post-date">7 March 2022</a>
+                    </div>
+                  </div>
+                  <div class="body">
+                    <div class="post-title"><a href="blog-single.html">Fabista Solutions</a></div>
+                    <div class="post-excerpt">Graphics Designer</div>
+                  </div>
+                  <div class="footer">
+                    <a href="blog-single.html">Read More <span class="mai-chevron-forward text-sm"></span></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-4 py-3">
+                <div class="card-blog">
+                  <div class="header">
+                    <div class="avatar">
+                      <img src="../assets/img/person/person_1.jpg" alt=""/>
+                    </div>
+                    <div class="entry-footer">
+                      <div class="post-author">Ayush Asawa</div>
+                      <a href="#" class="post-date">28 Apr 2021</a>
+                    </div>
+                  </div>
+                  <div class="body">
+                    <div class="post-title"><a href="blog-single.html">Solstice Computing</a></div>
+                    <div class="post-excerpt">Software Analyst</div>
+                  </div>
+                  <div class="footer">
+                    <a href="blog-single.html">Read More <span class="mai-chevron-forward text-sm"></span></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-4 py-3">
+                <div class="card-blog">
+                  <div class="header">
+                    <div class="avatar">
+                      <img src="../assets/img/person/person_1.jpg" alt=""/>
+                    </div>
+                    <div class="entry-footer">
+                      <div class="post-author">Ayush Asawa</div>
+                      <a href="#" class="post-date">28 Apr 2021</a>
+                    </div>
+                  </div>
+                  <div class="body">
+                    <div class="post-title"><a href="blog-single.html">Solstice Computing</a></div>
+                    <div class="post-excerpt">Software Analyst</div>
+                  </div>
+                  <div class="footer">
+                    <a href="blog-single.html">Read More <span class="mai-chevron-forward text-sm"></span></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-4 py-3">
+                <div class="card-blog">
+                  <div class="header">
+                    <div class="avatar">
+                      <img src="../assets/img/person/person_1.jpg" alt=""/>
+                    </div>
+                    <div class="entry-footer">
+                      <div class="post-author">Sam Newman</div>
+                      <a href="#" class="post-date">23 Apr 2020</a>
+                    </div>
+                  </div>
+                  <div class="body">
+                    <div class="post-title"><a href="blog-single.html">What is Business Management?</a></div>
+                    <div class="post-excerpt">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</div>
+                  </div>
+                  <div class="footer">
+                    <a href="blog-single.html">Read More <span class="mai-chevron-forward text-sm"></span></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 my-5">
+                <nav aria-label="Page Navigation">
+                  <ul class="pagination justify-content-center">
+                    <li class="page-item disabled">
+                      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                    </li>
+                    <li class="page-item active" aria-current="page">
+                      <a class="page-link" href="#">1 <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="page-item">
+                      <a class="page-link" href="#">2</a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                      <a class="page-link" href="#">Next</a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+              
+            </div>
+          </div>
+          
+        </div>
+      </div> 
+    </div>
+
+  </div>;
 };
 
 export default JobSeeker;
